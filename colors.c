2b6556b1294b2272e2f65cdb407541516ac881df
DeetSteve00 <98794456+DeetSteve00@users.noreply.github.com>
2022-02-01 13:11:20 -0600

    Create colors.c

---
+++ colors.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <windows.h>
+#include <string.h>
+#include <conio.h>
+
+int color(int color, char text[1000]) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+    printf(text);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
+}
+int main(){
+    color(/*Color goes here*/, /*Text goes here*/);
+return 0;
+}
